d817fed8bb1ac3ae71390d8be931fe014253ebc2
garrettkinman <garrettkinman@gmail.com>
2022-10-16 05:07:40 -0400

    algorithm is learning!
    TODO: plot data nicely for thesis and calculate avg time to TX for the differing discount factors

--- simulator.jl
+++ simulator.jl
@@ -1,5 +1,9 @@
 using NNlib
 using Plots
+using Logging
+using ProgressLogging
+using StatsBase
+
 
 "Convert continuous-valued state into discrete states using bucketing"
 function discretize(angle::AbstractFloat, duration::AbstractFloat, noise::Integer)
@@ -65,5 +69,124 @@ end
 "Represents an agent with a given preference model and current estimate of each state's value"
 mutable struct VirtualTransmitter
     pref_model::Function
-    value_estimates::AbstractArray{AbstractFloat,3} # should be 5×5×5 array, indexed as A×D×N
+    value_estimates::AbstractArray{AbstractFloat,4} # should be 5×5×5×n array, indexed as A×D×N×E
 end
+
+const STATE_SIZE = 5, 5, 5
+
+"Runs simulations for n epochs on a collection of transmitters with discount factor λ"
+function simulate!(transmitters::AbstractVector{VirtualTransmitter}, n_epochs=1000, λ=0.9)
+    @info "Beginning simulation!"
+
+    # use 100 satellite passes in 48 hours, which is roughly accurate to real life
+    n_passes = 100
+
+    # for storing tx success/failure results over the epochs
+    results = zeros(length(transmitters), n_epochs)
+
+    @withprogress for (i, txᵢ) ∈ enumerate(transmitters)
+        # record how many times each state was visited
+        s_counts = zeros(STATE_SIZE)
+
+        for epochᵢ ∈ 1:n_epochs
+            # TODO: choose just one value for noise each epoch?
+            # s̄ = [((75 * rand()) + 15, (50 * rand()) + 10, rand(-107:-93)) for j ∈ 1:n_passes]
+            s̄ = [((75 * rand()) + 15, (50 * rand()) + 10, -106) for j ∈ 1:n_passes]
+            t̄ = rand(n_passes) .* 48
+            v̄ = [txᵢ.value_estimates[discretize(sᵢ...)..., epochᵢ] for sᵢ ∈ s̄] # corresponding value estimates for each discretized pass
+
+            # calculate all the probabilities according to the policy π
+            probabilities = (λ .^ (t̄)) .* v̄ |> softmax
+
+            # randomly select a pass, weighted according to the policy π
+            selected_pass = sample(1:n_passes, Weights(probabilities))
+
+            # now that a pass is selected, store its indices into the value approximator
+            # and update state visit count
+            discretized_s = discretize(s̄[selected_pass]...)
+            s_counts[discretized_s...] += 1.0
+
+            # simulate tx for the selected pass
+            tx_result = (rand() < txᵢ.pref_model(s̄[selected_pass]...)) * 1.0
+
+            results[i, epochᵢ] = tx_result
+
+            # update estimates
+            if epochᵢ != n_epochs
+                # estimates should remain unchanged for all
+                txᵢ.value_estimates[:, :, :, epochᵢ + 1] = txᵢ.value_estimates[:, :, :, epochᵢ]
+                
+                # except for the one pass selected
+                vₙ = txᵢ.value_estimates[discretized_s..., epochᵢ] # current estimate
+                vₙ₊₁ = vₙ + (1 / (s_counts[discretized_s...] + 1)) * (tx_result - vₙ) # new estimate
+                txᵢ.value_estimates[discretized_s..., epochᵢ + 1] = vₙ₊₁
+            end
+        end
+
+        @logprogress i / length(transmitters)
+    end
+
+    return results
+end
+
+"Runs non-learning simulations for n epochs on a collection of transmitters"
+function baseline(transmitters::AbstractVector{VirtualTransmitter}, n_epochs=1000)
+    @info "Beginning simulation!"
+
+    # use 100 satellite passes in 48 hours, which is roughly accurate to real life
+    n_passes = 100
+
+    # for storing tx success/failure results over the epochs
+    results = zeros(length(transmitters), n_epochs)
+
+    @withprogress for (i, txᵢ) ∈ enumerate(transmitters)
+        for epochᵢ ∈ 1:n_epochs
+            # TODO: choose just one value for noise each epoch?
+            # s̄ = [((75 * rand()) + 15, (50 * rand()) + 10, rand(-107:-93)) for j ∈ 1:n_passes]
+            s̄ = [((75 * rand()) + 15, (50 * rand()) + 10, -106) for j ∈ 1:n_passes]
+
+            # randomly select a pass, weighted evenly
+            selected_pass = rand(1:n_passes)
+
+            # simulate tx for the selected pass
+            tx_result = (rand() < txᵢ.pref_model(s̄[selected_pass]...)) * 1.0
+
+            results[i, epochᵢ] = tx_result
+        end
+
+        @logprogress i / length(transmitters)
+    end
+
+    return results
+end
+
+# construct 100 virtual transmitters with the first preference model
+# optimistic initialization (i.e., the value function approximator begins as all ones)
+transmitters₁ = [VirtualTransmitter(pref_model₁, ones(STATE_SIZE..., 10000)) for i ∈ 1:100];
+
+results₁ = simulate!(transmitters₁, 10000, 0.99)
+baseline_results₁ = baseline(transmitters₁, 10000)
+
+results₁ |> mean
+results₁[:,1:1000] |> mean
+results₁[:,9000:10000] |> mean
+
+baseline_results₁ |> mean
+baseline_results₁[:,1:1000] |> mean
+baseline_results₁[:,9000:10000] |> mean
+
+transmitters₁[1].value_estimates[:,:,:,10000]
+
+transmitters₂ = [VirtualTransmitter(pref_model₂, ones(STATE_SIZE..., 10000)) for i ∈ 1:100];
+results₂ = simulate!(transmitters₂, 10000, 0.99)
+baseline_results₂ = baseline(transmitters₂, 10000)
+
+results₂ |> mean
+results₂[:,1:1000] |> mean
+results₂[:,9000:10000] |> mean
+
+baseline_results₂ |> mean
+baseline_results₂[:,1:1000] |> mean
+baseline_results₂[:,9000:10000] |> mean
+
+transmitters₂[1].value_estimates[:,:,:,10000]
